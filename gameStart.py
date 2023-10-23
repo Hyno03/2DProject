2a36a7ff5a82ed8a60ec9ffea7ce9e312a990e10
Hyno03 <jenny2117@naver.com>
2023-10-24 02:40:29 +0900

    Play button disappear
    when button has clicked

--- gameStart.py
+++ gameStart.py
@@ -1,12 +1,17 @@
 from pico2d import load_image
 
+import game_world
+
 
 class GameStart:
     def __init__(self):
         self.image = load_image('play.png')
+        self.x, self.y = 400, 200
+        self.clicked = False
 
     def draw(self):
-        self.image.draw(400, 200, 435/3, 120/3)
+        self.image.draw(self.x, self.y, 435/3, 120/3)
 
     def update(self):
-        pass
+        if self.clicked:
+            game_world.remove_object(self)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from gameStart import GameStart
 
 
 def handle_events():
+    global mouse_x, mouse_y
     global running
 
     events = get_events()
@@ -13,9 +14,15 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        # elif event.type == SDL_MOUSEMOTION:
+        #     mouse_x, mouse_y = event.x, 600 - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                gameStart.clicked = True
 
 
 def reset_world():
+    global mouse_x, mouse_y
     global running
     global gameStart
 
@@ -23,6 +30,8 @@ def reset_world():
 
     gameStart = GameStart()
     game_world.add_object(gameStart, 0)
+
+    mouse_x, mouse_y = 0, 0
 
 
 def update_world():
